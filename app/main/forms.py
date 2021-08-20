5661936ad23fb93f7b036dfdd5cc33f76342c92e
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-20 12:23:52 +0300

    Update forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from typing import Text
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField, SelectField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email,EqualTo, Length
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 
@@ -16,8 +16,12 @@ class UpdateProfile(FlaskForm):
 class BlogForm(FlaskForm):
     title = StringField('Enter the title of your blog',validators=[Required()])
     blog = TextAreaField('Enter your blog content',validators=[Required()])
-    submit = SubmitField('Post')
+    # submit = SubmitField('Post')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Add a comment', validators=[Required()])
+    blog_comment = TextAreaField('Add a comment', validators=[Required()])
     submit = SubmitField('Post')
+
+class SubscriberForm(FlaskForm):
+    subscriber_email = StringField('Email Address',validators=[Required(),Email()])
+    # submit = SubmitField('subscribe')
